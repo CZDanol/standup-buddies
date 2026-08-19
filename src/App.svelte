f520b0aa13461934ca4ead81ef89426148777eb3
Danol <czdanol@gmail.com>
2026-08-19 19:14:58 +0200

    Basic scaffolding

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<section class="section">
+  <div class="container is-max-desktop">
+    <h1 class="title">Standupbuddies</h1>
+    <p class="subtitle">Daily standup order &amp; talking time</p>
+  </div>
+</section>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import 'bulma/css/bulma.min.css'
+import { mount } from 'svelte'
+import App from './App.svelte'
+
+mount(App, {
+  target: document.getElementById('app')!,
+})
