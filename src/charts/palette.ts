31045cd384946f5906f92abfcbcc539caf448938
Danol <czdanol@gmail.com>
2026-08-19 23:41:08 +0200

    Tweak palette a bit

--- src/charts/palette.ts
+++ src/charts/palette.ts
@@ -1,25 +1,26 @@
 import { SpecialSpeakingState } from "../domain/types";
 import { meeting, type SpeakingState } from "../meeting.svelte";
 
+// Sasha Trubetskoy's distinct-colors list, pre-darkened to 80% for the dark theme.
 const attendeeColors = [
-  "#e6194B",
-  "#3cb44b",
-  "#ffe119",
-  "#4363d8",
-  "#f58231",
-  "#911eb4",
-  "#42d4f4",
-  "#f032e6",
-  "#bfef45",
-  "#fabed4",
-  "#469990",
-  "#dcbeff",
-  "#9A6324",
-  "#fffac8",
-  "#800000",
-  "#aaffc3",
-  "#808000",
-  "#ffd8b1",
+  "#b8143c",
+  "#30903c",
+  "#ccb414",
+  "#364fad",
+  "#c46827",
+  "#741890",
+  "#35aac3",
+  "#c028b8",
+  "#99bf37",
+  "#c898aa",
+  "#387a73",
+  "#b098cc",
+  "#7b4f1d",
+  "#ccc8a0",
+  "#660000",
+  "#88cc9c",
+  "#666600",
+  "#ccad8e",
 ];
 
 /** Color of the given speaking state, identical across all charts. */
